f32397cd81de945616278cb22b398892db964219
Dmitriy Donskikh <dndonskih@gmail.com>
2022-05-26 22:03:07 +0300

    add c# snippet

--- client.cs
+++ client.cs
@@ -1,34 +1,4 @@
-﻿
-/*
- 
- using System;
-using System.Threading;
-using NetMQ;
-using NetMQ.Sockets;
-
-static class Program
-{
-    public static void Main()
-    {
-        using (var responder = new ResponseSocket())
-        {
-            responder.Bind("tcp://*:5555");
-
-            while (true)
-            {
-                string str = responder.ReceiveFrameString();
-                Console.WriteLine($"Received: {str}");
-                Thread.Sleep(1000);  //  Do some 'work'
-                responder.SendFrame("World");
-            }
-        }
-    }
-}
- */
-
-
-
-using System;
+﻿using System;
 using NetMQ;
 using NetMQ.Sockets;
 
