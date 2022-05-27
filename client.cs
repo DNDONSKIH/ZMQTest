9df8255fafb52f26259da8a3d6dac576d2e0109d
Dmitriy Donskikh <dndonskih@gmail.com>
2022-05-27 08:55:25 +0300

    fix c# snippet

--- client.cs
+++ client.cs
@@ -1,6 +1,5 @@
 ﻿
 
-
 using System;
 using NetMQ;
 using NetMQ.Sockets;
@@ -9,6 +8,24 @@ using System.Text.Json;
 
 namespace ConsoleApp1
 {
+    class MeasData
+    {
+        public string? instrument { get; set; }
+        public string? meas { get; set; }
+        public int src1 { get; set; }
+        public int src2 { get; set; }
+        public double result { get; set; }
+
+        public MeasData(string? instrument, string? meas, int src1, int src2, double result)
+        {
+            this.instrument = instrument;
+            this.meas = meas;
+            this.src1 = src1;
+            this.src2 = src2;
+            this.result = result;
+        }
+    }
+
     static class Program
     {
         public static void Main()
@@ -21,16 +38,19 @@ namespace ConsoleApp1
                 int requestNumber;
                 for (requestNumber = 0; requestNumber != 10; requestNumber++)
                 {
-                    Console.WriteLine("Sending Hello {0}...", requestNumber);
+                    Console.WriteLine("Sending Msg {0}...", requestNumber);
 
-                    int src1 = 2;
-                    int src2 = 5;
+                    //int src1 = 2;
+                    //int src2 = 5;
 
-                    string message = "{" + "\"instrument\"  :\"rto\", " +
-                                                "\"meas\"   :\"phase\", " +
-                                               $"\"src1\"        :\"{src1}\", " +
-                                               $"\"src2\"        :\"{src2}\" " + "}";
+                    //string message = "{" + "\"instrument\"  :\"rto\", " +
+                    //                            "\"meas\"   :\"phase\", " +
+                    //                           $"\"src1\"        :\"{src1}\", " +
+                    //                           $"\"src2\"        :\"{src2}\" " + "}";
 
+
+                    MeasData meas = new MeasData("rto", "phase", 9 , 8, 0.0);
+                    string message = JsonSerializer.Serialize(meas);
 
                     requester.SendFrame(message);
                     string response = requester.ReceiveFrameString();
@@ -43,7 +63,6 @@ namespace ConsoleApp1
         }
     }
 }
-
 
 
 
